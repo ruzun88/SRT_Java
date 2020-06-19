318f2e207ed57af85ab240c7a768f64b78017f05
ruzun88 <ruzun88@gmail.com>
2020-06-19 23:57:42 +0900

    pay, train 객체 추가

---
+++ src/main/java/kor/ruzun88/java/srt/model/Pay.java
@@ -0,0 +1,8 @@
+package kor.ruzun88.java.srt.model;
+
+import lombok.Data;
+
+@Data
+public class Pay {
+    Object stlbTrnClsfCd, trnNo, dptDt, dptTm, dptRsStnCd, arvTm, arvRsStnCd, iseLmtDt, iseLmtTm;
+}
---
+++ src/main/java/kor/ruzun88/java/srt/model/Train.java
@@ -0,0 +1,8 @@
+package kor.ruzun88.java.srt.model;
+
+import lombok.Data;
+
+@Data
+public class Train {
+    Object pnrNo, rcvdAmt, tkSpecNum;
+}

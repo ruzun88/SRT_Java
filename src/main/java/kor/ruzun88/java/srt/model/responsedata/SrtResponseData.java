05f9f2b3598559619c8fa6f4c9951592ce26a2e4
ruzun88 <ruzun88@gmail.com>
2020-06-22 23:56:43 +0900

    no message

--- src/main/java/kor/ruzun88/java/srt/model/responsedata/SrtResponseData.java
+++ src/main/java/kor/ruzun88/java/srt/model/responsedata/SrtResponseData.java
@@ -1,4 +1,7 @@
 package kor.ruzun88.java.srt.model.responsedata;
 
 public class SrtResponseData {
+    private static final String STATUS_SUCCESS = "SUCC";
+    private static final String STATUS_FAIL = "FAIL";
+
 }
